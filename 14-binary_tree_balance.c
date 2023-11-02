1fc0be8bc29f0bd14335b4f35c84ed854da3f243
douniabendib <bendib.dounia@gmail.com>
2023-11-02 14:02:11 -0700

    balance tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,6 +25,7 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_h = 0, right_h = 0;
+
 	if (tree == NULL)
 		return (0);
 	left_h = binary_tree_height_b(tree->left) + 1;
