8de24d5f79d47375dee12abf7310783f6f62e7e9
douniabendib <bendib.dounia@gmail.com>
2023-11-02 07:48:04 -0700

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth-function that measures the depth
+ * of a node in a binary tree
+ * @tree:pointer to the node to measure the depth
+ * Return:0 or depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	depth = 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	while (tree->parent != NULL)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+	return (depth);
+}
