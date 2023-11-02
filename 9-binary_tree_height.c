c407cb73addab3aa142aeda23a25c5def7c41fcf
douniabendib <bendib.dounia@gmail.com>
2023-11-02 05:44:04 -0700

    update height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t Left_height, Right_height;
+	size_t Left_height = 0, Right_height = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
-	Left_height  = binary_tree_height(tree->left);
-	Right_height = binary_tree_height(tree->right);
-	if (Left_height > Right_height)
-		return (Left_height + 1);
+	}
 	else
-		return (Right_height + 1);
+	{
+		Left_height = binary_tree_height(tree->left);
+		Right_height = binary_tree_height(tree->right);
+		if ( Right_height > Left_height)
+			return (Right_height + 1);
+		else
+			return (Left_height + 1);
+	}
 }
