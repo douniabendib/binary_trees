45692844dfe91cd1d2de8f2866f74849fe5c9cf6
douniabendib <bendib.dounia@gmail.com>
2023-11-02 09:22:38 -0700

    count node

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes-  function that counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes-function that counts the nodes with at least 1
+ * child in a binary tree
  * @tree:pointer to the root node of the tree to count the number of nodes
  * Return: 0 or num of node
  */
@@ -11,6 +12,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 	if (tree->right || tree->left)
-		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) +1);
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 	return (0);
 }
