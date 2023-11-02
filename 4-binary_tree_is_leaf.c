d8deae6d3002a1b900d3437fea64310349392ad9
douniabendib <bendib.dounia@gmail.com>
2023-11-01 17:38:10 -0700

    root

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
+ * binary_tree_is_leaf- function that checks if a node is a leaf
  * @node: pointer to the node to check
- * return: 0 or 1
+ * Return: 0 or 1
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
